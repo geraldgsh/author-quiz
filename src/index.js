32f81ba042227f20dfc02e3be64efeae34eb9f61
geraldgsh <geraldgsh@hotmail.com>
2020-04-06 12:37:00 +0800

    added more authors and books

--- src/index.js
+++ src/index.js
@@ -6,16 +6,50 @@ import * as serviceWorker from './serviceWorker';
 
 const authors = [
   {
-    name: 'Mark Twain',
-    imageUrl: 'images/authors/marktwain.jpg',
-    imageSource: 'Wikimedia Commons',
-    books: [
-        'The Adventures of Huckleberry Finn',
-        'Life on the Mississippi',
-        'Roughing It'
-    ]
-  }  
-]
+      name: 'Mark Twain',
+      imageUrl: 'images/authors/marktwain.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: [
+          'The Adventures of Huckleberry Finn',
+          'Life on the Mississippi',
+          'Roughing It'
+      ]
+  },
+  {
+      name: 'Joseph Conrad',
+      imageUrl: 'images/authors/josephconrad.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['Heart of Darkness']
+  },
+  {
+      name: 'J.K. Rowling',
+      imageUrl: 'images/authors/jkrowling.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Daniel Ogren',
+      books: ['Harry Potter and the Sorcerers Stone']
+  },
+  {
+      name: 'Stephen King',
+      imageUrl: 'images/authors/stephenking.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Pinguino',
+      books: [
+          'The Shining',
+          'It'
+      ]
+  },
+  {
+      name: 'William Shakespeare',
+      imageUrl: 'images/authors/shakespeare.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: [
+          'Romeo and Juliet',
+          'Hamlet',
+          'Otello',
+          'Macbeth'
+      ]
+  }
+];
 
 const state = {
   turnData: {
